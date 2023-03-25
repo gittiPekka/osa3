3990743c1b1dcb3267c5a089ac524647798e9aea
Pekka Paldanius <pekka.paldanius@kotiposti.net>
2023-03-25 13:09:36 +0200

    some logs removed

--- puhelinluettelo/src/components/Persons.js
+++ puhelinluettelo/src/components/Persons.js
@@ -2,9 +2,6 @@ import Person from './Person'
 
 const Persons = (props) => {
     const personsToShow = props.persons.filter(person => person.name.includes(props.filterPersons));
-    // const personsToShow = props.persons;
-    // console.log("props: ", props);
-    // console.log("props.filterPersons: ", props.filterPersons);
     return (
         <ul>
         {personsToShow.map(person =>
